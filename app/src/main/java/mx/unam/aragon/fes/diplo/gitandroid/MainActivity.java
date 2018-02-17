b08d435f418ec835966b699ceda69af8b9d15ad3
niodhoggr <jorge.zuniga0910@gmail.com>
2018-02-17 00:33:11 -0600

    Se agrega boton  y funcionalidad del mismo: al dar click al boton despliega mensaje

--- app/src/main/java/mx/unam/aragon/fes/diplo/gitandroid/MainActivity.java
+++ app/src/main/java/mx/unam/aragon/fes/diplo/gitandroid/MainActivity.java
@@ -1,13 +1,36 @@
     package mx.unam.aragon.fes.diplo.gitandroid;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+    import android.os.Bundle;
+    import android.support.v7.app.AppCompatActivity;
+    import android.util.Log;
+    import android.view.View;
+    import android.widget.Button;
+    import android.widget.EditText;
+    import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public class MainActivity extends AppCompatActivity {
+        private Button boton;
+        private TextView txtSalida;
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            boton=findViewById(R.id.boton);
+            txtSalida=findViewById(R.id.txtLabel);
+
+
+            boton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.v("diplo", "Evento lanzado" + txtSalida.getText());
+                    txtSalida.setText("Hola Git");
+
+                }
+
+            });
+
+        }
     }
-}
